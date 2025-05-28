3603a46ad747732ccdfd7e6272b661f4fd5ecc61
felipenewplayer <felipeapereira2014@gmail.com>
2025-05-28 17:51:24 -0300

    arrumando

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,25 @@
+const button = document.getElementById("button");
+const menu = document.getElementById("teste");
+const botaoFechar = document.getElementById("fechar-menu");
+button.addEventListener('click', () =>{
+    const button = document.querySelector("button");
+    button.classList.add("hidden");
+    if(menu.classList.contains("desativado")){
+        menu.classList.remove("desativado");
+        menu.classList.add("ativado");
+    }else {
+        menu.classList.remove("ativado");
+        menu.classList.add("desativado");
+    }
+})
+
+botaoFechar.addEventListener("click", () =>{
+    menu.classList.remove("ativado");
+    menu.classList.add("desativado");
+    const button = document.querySelector("button");
+    button.classList.remove("hidden");
+})
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry =>{
         if(entry.isIntersecting){
